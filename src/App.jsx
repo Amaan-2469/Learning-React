d191401113c64997bb57cba8ec59f990bd6af258
Amaan-2469 <amaan.ansari2469@gmail.com>
2025-09-14 15:51:32 +0530

    component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Login, {Profile, Setting} from "./UserComponent"
+function App(){
+  return(
+    <div>
+      <h1>Hello World</h1>
+      <Login />
+      <Profile />
+      <Setting />
+    </div>
+  )
+}
+
+export default App
