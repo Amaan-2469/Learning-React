7e82b137e2683273fad67e73cb6237db591f644f
Amaan-2469 <amaan.ansari2469@gmail.com>
2025-09-14 16:36:53 +0530

    Worked with JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 function App(){
+  const name = "Amaan Ansari"
+  let x = 10 
+  let y = 20
   return(
-    <div>
-      <h1>Learnig React</h1>
-    </div>
+    <>
+     <h1>{name}</h1>
+     <h1>{10 + 20 + 30}</h1> 
+     <h1>{ x + y }</h1>
+     <button onClick={() => alert("Hello")}>Click Me</button>
+    </>
   )
 }
 
